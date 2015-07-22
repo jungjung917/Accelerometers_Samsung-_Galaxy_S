3e737c72d614627f579e0412cfe258141be6cad6
Zhengshi Lucia <zhengshizhao917@gmail.com>
2015-07-21 23:33:16 -0400

    R program to download data

--- get_data.R
+++ get_data.R
@@ -1,5 +1,5 @@
 ## download data files and unzip the data
-#setwd("/Users/zhengshizhao/lecture_yale/courseea_DEC/part2_JAN/Get&CleanDAta/projects")
+
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 destfile <- "./dataset.zip"
 method <- "curl"
