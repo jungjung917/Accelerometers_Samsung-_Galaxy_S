2676d77f74bf438fb564a13a833d61c3d0cf39f9
Zhengshi Lucia <zhengshizhao917@gmail.com>
2015-07-22 01:02:11 -0400

    Main program

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,7 @@
 #source("get_data.R")
-
+setwd("./Accelerometers_Samsung_Galaxy_S")
 ###read data from data files
-
+library(dplyr)
 X_test <- read.table(file = "./dataset//UCI HAR Dataset/test/X_test.txt")
 Y_test <- read.table(file = "./dataset//UCI HAR Dataset/test/Y_test.txt")
 subject_test <-read.table(file = "./dataset//UCI HAR Dataset/test/subject_test.txt")
@@ -9,10 +9,11 @@ subject_train <- read.table(file = "./dataset//UCI HAR Dataset/train/subject_tra
 X_train <- read.table(file = "./dataset//UCI HAR Dataset/train/X_train.txt")
 Y_train <- read.table(file = "./dataset//UCI HAR Dataset/train/Y_train.txt")
 feature <- read.table("./dataset//UCI HAR Dataset/features.txt") 
-active <- read.table("./dataset//UCI HAR Dataset/activity_labels.txt")
-## step 1 : merge test set and train set and give the column names 
+activity <- read.table("./dataset//UCI HAR Dataset/activity_labels.txt")
 
+## step 1 : merge test set and train set and give the column names 
 total_set <- rbind(cbind(subject_test,Y_test,X_test),cbind(subject_train,Y_train,X_train))
+rm(list = c('X_test','Y_test','subject_test','X_train','Y_train','subject_train'))
 names(total_set) <- c("SubjectID", "Activity",as.character(feature[,2]))
 
 ## step 2: mean_sd_set file includes only the measurements 
@@ -23,8 +24,16 @@ meanid <- grep("mean",names(total_set))
 mean_sd_set <- total_set[,c(1,2,meanid,sdid)]
 
 ## step3 
-mergeData <- merge(active, mean_sd_set,by.x="V1",by.y="Activity")
+mergeData <- merge(activity, mean_sd_set,by.x="V1",by.y="Activity")
+rm(mean_sd_set,meanid,sdid,activity,feature)
 
 ## step 4
-
+names(mergeData)[2]<- "Activity" 
+mergeData <- select(mergeData,SubjectID,Activity,4:ncol(mergeData),-V1)
 ## step 5 
+mergeData %>% 
+        group_by(SubjectID,Activity) %>%
+        summarise_each(funs(mean),3:ncol(mergeData)) %>% 
+        write.csv(file="mean_table.csv")
+mean_table<- read.csv("mean_table.csv")
+head(mean_table,n=10)
